6bbae88c2f11f0e8a277eaee6cd5d7fa713dda87
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-10-27 18:52:37 +0100

    Creato SetComparator e modificato UseSetWithOrder

---
+++ src/it/unibo/oop/lab05/ex2/SetComparator.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.lab05.ex2;
+
+import java.util.*;
+
+public class SetComparator implements Comparator<String>{
+
+	public SetComparator() {}
+
+	public int compare(String arg0, String arg1) {
+		return Integer.compare(Integer.parseInt(arg0), Integer.parseInt(arg1));
+	}
+
+}
