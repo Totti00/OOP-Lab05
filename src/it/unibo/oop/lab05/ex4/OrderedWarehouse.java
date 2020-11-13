86e86eca615b859527c7c65b9a4dc41319fd8012
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-13 20:45:22 +0100

    prova

--- src/it/unibo/oop/lab05/ex4/OrderedWarehouse.java
+++ src/it/unibo/oop/lab05/ex4/OrderedWarehouse.java
@@ -1,5 +1,14 @@
 package it.unibo.oop.lab05.ex4;
 
-public class OrderedWarehouse {
+import java.util.Set;
+import java.util.TreeSet;
 
+import it.unibo.oop.lab05.ex3.Product;
+import it.unibo.oop.lab05.ex3.WharehouseImpl;
+
+public class OrderedWarehouse extends WharehouseImpl{
+	public Set<Product> allProducts(){
+		return new TreeSet<>(super.allProducts());
+		
+	}
 }
