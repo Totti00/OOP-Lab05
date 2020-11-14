86163c7d9db9e1ca6fac2a556a240bb8625e501a
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-14 13:08:51 +0100

    Modificato Utilities

--- src/it/unibo/oop/lab05/ex5/Utilities.java
+++ src/it/unibo/oop/lab05/ex5/Utilities.java
@@ -1,6 +1,7 @@
 package it.unibo.oop.lab05.ex5;
 
 import java.util.Collection;
+import java.util.Random;
 
 /**
  *
@@ -21,6 +22,7 @@ public final class Utilities {
      *            Collection type
      */
     public static <X> void copyAll(final Collection<X> source, final Collection<? super X> target) {
+    target.addAll(source);
     }
 
     /**
@@ -32,7 +34,14 @@ public final class Utilities {
      * 
      */
     public static <X> X getRandomElement(final Collection<X> coll) {
-        return null;
+        int random = new Random().nextInt(coll.size());
+        for (final X x: coll) {
+        	if(random == 0) {
+        		return x;
+        	}
+        	random--;
+        }
+    	return null;
     }
 
     /**
@@ -47,6 +56,10 @@ public final class Utilities {
      * @return a pair with two random elements
      */
     public static <X, Y> Pair<X, Y> getRandomPair(final Collection<X> one, final Collection<Y> two) {
-        return null;
+    	final Pair<X,Y> p = new Pair<>(
+    				getRandomElement(one), 
+    				getRandomElement(two)
+    				);
+    	return p;
     }
 }
