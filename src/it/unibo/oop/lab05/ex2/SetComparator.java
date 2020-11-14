ed757ac2bbf16cbfb227ece0d5e1b35318689061
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-14 11:17:39 +0100

    Modificato

--- src/it/unibo/oop/lab05/ex2/SetComparator.java
+++ src/it/unibo/oop/lab05/ex2/SetComparator.java
@@ -4,10 +4,49 @@ import java.util.*;
 
 public class SetComparator implements Comparator<String>{
 
-	public SetComparator() {}
-
+	
 	public int compare(String arg0, String arg1) {
 		return Integer.compare(Integer.parseInt(arg0), Integer.parseInt(arg1));
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*public SetComparator() {}
+
+	public int compare(String arg0, String arg1) {
+		return Integer.compare(Integer.parseInt(arg0), Integer.parseInt(arg1));
+	}*/
 
 }
--- src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -20,19 +20,21 @@ public final class UseSetWithOrder {
          * 
          * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
          * a new Comparator in a separate class.
-         * 
-         * 2) Inserts in the set a hundred strings containing randomly generated
-         * numbers (use Math.random())
-         * 
-         * 3) Prints the set, which must be ordered
          */
+    	
     	final Set<String> set = new TreeSet<>(new SetComparator());
-    	for(int i = 0; i<100; i++) {
-    		set.add(Integer.toString((int)(Math.random()*1000)));
+         
+    	/* 2) Inserts in the set a hundred strings containing randomly generated
+         * numbers (use Math.random())
+         */
+    	
+    	for(int i = 0; i < 101; i++) {
+    		set.add(Integer.toString((int)(Math.random() * 1000)));
     	}
     	
-    	for(final String s: set) {
-    		System.out.print(s + ",");
-    	}
+         /* 3) Prints the set, which must be ordered
+         */
+    	
+    	System.out.print(set);
     }
 }
